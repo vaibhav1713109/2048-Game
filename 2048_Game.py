23e74542e173768efe17aedbeba553504239efd3
Vaibhav <vaibhav1713109@akgec.ac.in>
2020-08-13 01:46:49 +0530

    Add .py files

---
+++ 2048_Game.py
@@ -0,0 +1,75 @@
+from tkinter import Frame,Label,CENTER
+
+import game_logic
+import constant as c
+
+#inherite the frame class
+class Game2048(Frame):
+    def __init__(self):
+        #inherite the fram_init function
+        Frame.__init__(self)                #by using this we make a frame
+
+        self.grid()                         #the frame is visiulise as grid
+        self.master.title('2048')           #it's the title of game at the center of frame
+        #self.mater.bind is used if any key is pressed then it goes to self.key_dowm function
+        self.master.bind("<Key>",self.key_down)
+        self.commands={c.KEY_UP:game_logic.move_up, c.KEY_DOWN:game_logic.move_down,c.KEY_LEFT:game_logic.move_left, c.KEY_RIGHT:game_logic.move_right}
+
+        self.grid_cells=[]                   #in the given grid their are some which is initially empty
+        self.init_grid()                     #initialize the grid and add the grid cells
+        self.init_matrix()                   #it will create the matrix of 4*4
+        self.update_grid_cells()             #it upgrade the grid cells according to the no present in grid cells
+
+        self.mainloop()                      #it run the programm
+
+    def init_grid(self):
+        #inside a frame we make another frame(400*400) which is type of visit(button),here self means previous frame
+        backround=Frame(self, bg=c.BACKGROUND_COLOR_GAME,width=c.SIZE,height=c.SIZE)
+        backround.grid()
+
+        for i in range(c.GRID_LEN):
+            grid_row=[]
+            for j in range(c.GRID_LEN):
+                #inside the background we make small size(100*100) of frames(cell)
+                cell=Frame(backround,bg=c.BACKGROUND_COLOR_CELL_EMPTY,width=c.SIZE/c.GRID_LEN,height=c.SIZE/c.GRID_LEN)
+                #cell.grid means inside the grid we gave padding beteween cells
+                cell.grid(row=i,column=j,padx=c.GRID_PADDING,pady=c.GRID_PADDING)
+
+                #label means we add text (2,4,8) 
+                t=Label(master=cell,text="",bg=c.BACKGROUND_COLOR_CELL_EMPTY,justify=CENTER,font=c.FONT,width=5,height=2)
+                t.grid()
+                grid_row.append(t)
+            self.grid_cells.append(grid_row)
+
+    def init_matrix(self):
+        self.matrix=game_logic.start_game()
+        game_logic.add_new_2(self.matrix)
+        game_logic.add_new_2(self.matrix)
+
+    def update_grid_cells(self):
+        for i in range(c.GRID_LEN):
+            for j in range(c.GRID_LEN):
+                new_number=self.matrix[i][j]
+                if new_number==0:
+                    self.grid_cells[i][j].configure(text="",bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                else:
+                    self.grid_cells[i][j].configure(text=str(new_number),bg=c.BACKGROUND_COLOR_DICT[new_number],fg=c.CELL_COLOR_DICT[new_number])
+
+        self.update_idletasks()             #it will wait untill all the color are changed
+
+    def key_down(self,event):
+        key=repr(event.char)                #it give exect value(like "'w'")
+        if key in self.commands:
+            self.matrix,changed=self.commands[repr(event.char)](self.matrix)
+            if changed:
+                game_logic.add_new_2(self.matrix)
+                self.update_grid_cells()
+                changed=False
+                if game_logic.get_current_state(self.matrix)=="WON":
+                    self.grid_cells[1][1].configure(text="You",bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[1][2].configure(text="Win",bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                if game_logic.get_current_state(self.matrix)=="Lost":
+                    self.grid_cells[1][1].configure(text="You",bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+                    self.grid_cells[1][2].configure(text="Lose!",bg=c.BACKGROUND_COLOR_CELL_EMPTY)
+
+gamegrig=Game2048()
---
+++ game_logic.py
@@ -0,0 +1,134 @@
+import random
+
+def start_game():
+    mat=[]
+    for i in range(4):
+        mat.append([0]*4)
+    return mat
+
+def add_new_2(mat):
+    r=random.randint(0,3)
+    c=random.randint(0,3)
+    while mat[r][c]!=0:
+        r=random.randint(0,3)
+        c=random.randint(0,3)
+    mat[r][c]=2
+
+def get_current_state(mat):
+    for i in range(4):
+        for j in range(4):
+            if mat[i][j]==2048:
+                return 'WON'
+    
+    for i in range(4):
+        for j in range(4):
+            if mat[i][j]==0:
+                return 'Game not end'
+
+    for i in range(3):
+        for j in range(3):
+            if mat[i][j]==mat[i][j+1] or mat[i][j]==mat[i+1][j]:
+                return 'Game not end'
+    
+    for i in range(3):
+        if mat[i][3]==mat[i+1][3]:
+            return 'Game not end'
+
+    for j in range(3):
+        if mat[3][j]==mat[3][j+1]:
+            return 'Game not end'
+
+    return 'Lost'
+
+def compress(mat):
+    new_mat=[]
+    for i in range(4):
+        new_mat.append([0]*4)
+
+    changed=False
+    for i in range(4):
+        pos=0
+        for j in range(4):
+            if mat[i][j]!=0:
+                new_mat[i][pos]=mat[i][j]
+                if j!=pos:
+                    changed=True
+                pos+=1
+    
+    return new_mat,changed
+
+def merge(mat):
+    changed=False
+    for i in range(4):
+        for j in range(3):
+            if mat[i][j]==mat[i][j+1] and mat[i][j]!=0:
+                changed=True
+                mat[i][j]=mat[i][j]*2
+                mat[i][j+1]=0
+    return mat,changed
+
+def reverse(mat):
+    new_mat=[]
+    for i in range(4):
+        new_mat.append([])
+        for j in range(4):
+            new_mat[i].append(mat[i][4-j-1])
+    return new_mat
+
+def transpos(mat):
+    new_mat=[]
+    for i in range(4):
+        new_mat.append([])
+        for j in range(4):
+            new_mat[i].append(mat[j][i])
+    return new_mat
+
+def move_left(mat):
+    new_mat,changed1=compress(mat)
+    new_mat,changed2=merge(new_mat)
+    changed=changed1 or changed2
+    final_mat,changed3=compress(new_mat)
+    return final_mat ,changed
+
+def move_right(mat):
+    reverse_mat=reverse(mat)
+    new_mat,changed1=compress(reverse_mat)
+    new_mat,changed2=merge(new_mat)
+    changed=changed1 or changed2
+    new_mat,changed3=compress(new_mat)
+    final_mat=reverse(new_mat)
+    return final_mat ,changed
+
+def move_up(mat):
+    transpos_mat=transpos(mat)
+    new_mat1,changed1=compress(transpos_mat)
+    new_mat2,changed2=merge(new_mat1)
+    changed=changed1 or changed2
+    new_mat3,changed3=compress(new_mat2)
+    final_mat=transpos(new_mat3)
+    return final_mat ,changed
+
+def move_down(mat):
+    transpos_mat=transpos(mat)
+    reverse_mat=reverse(transpos_mat)
+    new_mat,changed1=compress(reverse_mat)
+    new_mat,changed2=merge(new_mat)
+    changed=changed1 or changed2
+    new_mat,changed3=compress(new_mat)
+    new_mat=reverse(new_mat)
+    final_mat=transpos(new_mat)
+    return final_mat ,changed
+
+
+# mat=start_game()
+# print(mat)
+# add_new_2(mat)
+# add_new_2(mat)
+# add_new_2(mat)
+# add_new_2(mat)
+# add_new_2(mat)
+# print(mat)
+# m=move_down(mat)
+# print(m)
+
+
